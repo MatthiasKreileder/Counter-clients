d69256fbbb6be6acf55a4e0a5a1ae1e4cba24dbd
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-11-05 21:16:02 +0000

    initial dump

---
+++ Counter-iOS/Counter-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter-iOS
+//
+//  Created by Honza Dvorsky on 11/5/15.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
